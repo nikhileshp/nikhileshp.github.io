55dcae3c1311b788683521401280a95f937124c0
Nikhilesh <prabhakar.nikhilesh@gmail.com>
2023-05-29 19:41:31 -0500

    Temporary openkey

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -17,7 +17,7 @@ module ExternalPosts
       uri = URI.parse("https://api.notion.com/v1/databases/66e4dc0bf46f480a903408b7725e9ecd/query")
       request = Net::HTTP::Post.new(uri)
       request.content_type =  "application/json"
-      request["Authorization"] = ENV['NOTION_API_KEY']
+      # request["Authorization"] = ENV['NOTION_API_KEY']
       request["Authorization"] = 'secret_tn7WHwJcnH57na08yt52eTn0yiEXA6gp7cHE3J5mtyp'
       request["Notion-Version"] = "2022-06-28"
       request.body = JSON.dump({
